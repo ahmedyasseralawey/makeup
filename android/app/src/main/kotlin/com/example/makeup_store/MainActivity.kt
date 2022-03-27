2725dfcf7fe409462e449e7ac938050f38b2373b
ahmedyasseralawey <ahmedyasseralawey@gmail.com>
2022-03-27 02:11:55 +0200

    This is First Commit

---
+++ android/app/src/main/kotlin/com/example/makeup_store/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.makeup_store
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/controller/controller.dart
@@ -0,0 +1,27 @@
+//get-state
+import 'package:get/state_manager.dart';
+import 'package:makeup_store/model/product.dart';
+import 'package:makeup_store/service/serviceController.dart';
+
+class ProductController extends GetxController {
+  var isLoading = true.obs;
+  var productList = <Product>[].obs;
+
+  @override
+  void onInit() {
+    fetchProducts();
+    super.onInit();
+  }
+
+  void fetchProducts() async {
+    try {
+      isLoading(true);
+      var products = await RemoteServices.fetchProducts();
+      if (products != null) {
+        productList.value = products;
+      }
+    } finally {
+      isLoading(false);
+    }
+  }
+}
